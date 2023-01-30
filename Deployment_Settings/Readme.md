383695aa2110bb1aeab52b5879df71916314df14
salemmohammed <salemmoh@buffalo.edu>
2023-01-29 21:04:15 -0500

    shell script

--- Deployment_Settings/Readme.md
+++ Deployment_Settings/Readme.md
@@ -1,4 +1,20 @@
-# Experiment Settings and Deployments
+ded# Experiment Settings and Deployments
 
-First, install and build CRDB from source. Note that at least 4GB of RAM is required to build from source and run tests.
+
+Second, Run CRDB on the Cluster. 
+
+cockroach start --insecure --store=tpcc-local1 --listen-addr=localhost:26257 --http-addr=localhost:8080 --join=localhost:26257,localhost:26258,localhost:26259 --background
+
+cockroach start --insecure --store=tpcc-local2 --listen-addr=localhost:26258 --http-addr=localhost:8081 --join=localhost:26257,localhost:26258,localhost:26259 --background
+
+cockroach start --insecure --store=tpcc-local3 --listen-addr=localhost:26259 --http-addr=localhost:8082 --join=localhost:26257,localhost:26258,localhost:26259 --background
+
+cockroach init --insecure --host=localhost:26257
+
+cockroach workload fixtures import tpcc --warehouses=10 'postgresql://root@localhost:26257?sslmode=disable'
+
+
+cockroach workload run tpcc --warehouses=10 --ramp=3m --duration=1m 'postgresql://root@localhost:26257?sslmode=disable'
+
+
 
---
+++ Deployment_Settings/install.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+clear
+
+# update package list.
+sudo apt-get update
+# Install build-essential. This will install gcc
+sudo apt install build-essential
+# check g++ version
+sudo apt-get install g++
+g++ --version
+# Installing the ncurses library in Debian/Ubuntu Linux
+sudo apt-get install libncurses5-dev libncursesw5-dev
+# Install "libresolv-wrapper" Package on Ubuntu
+
+sudo apt-get update -y
+sudo apt-get install -y libresolv-wrapper
+# latest stable version of git
+sudo apt-get install git
+# install bash
+sudo apt-get install bash
+# install cmake
+sudo apt-get install cmake
+# install autoconf
+sudo apt-get install autoconf
+
+sudo apt-get install bison flex
+sudo apt-get install -y bison
+sudo apt-get install patch
+
+# bazelisk for bazel
+wget wget https://github.com/bazelbuild/bazelisk/releases/download/v1.8.1/bazelisk-linux-amd64
+chmod +x bazelisk-linux-amd64
+sudo rm -rf /usr/local/bazel
+sudo mv bazelisk-linux-amd64 /usr/local/bin/bazel
+which bazel
+
+wget https://dl.google.com/go/go1.17.7.linux-amd64.tar.gz 
+sudo tar -xvf go1.17.7.linux-amd64.tar.gz
+sudo mv go /usr/local
+# change the golang path
+echo export GOROOT=/usr/local/go
+echo export GOPATH=$HOME
+echo export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+go version
+
+# install CRDB source code  
+git clone https://github.com/salemmohammed/cockroach.git
+cd cockroach
+# build the source code from source
+./dev build
+./dev doctor
